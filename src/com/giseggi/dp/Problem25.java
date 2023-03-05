8d1daca146fe7bd473ca1576f8e6d982b8948787
giseggi <giseggi57@gmail.com>
2023-03-05 19:05:14 +0900

    Add comment

--- src/com/giseggi/dp/Problem25.java
+++ src/com/giseggi/dp/Problem25.java
@@ -1,9 +1,14 @@
 package com.giseggi.dp;
 
 import java.util.Scanner;
-
+/**
+ * Problem description
+ * https://www.acmicpc.net/problem/2502
+ */
+problem description
 public class Problem25 {
 	public static void main(String[] args) {
+		
         Scanner sc = new Scanner(System.in);
         int D = sc.nextInt();
         int K = sc.nextInt();
