814f63e4b01f09629dff021c2d1846e4f0654633
giseggi <giseggi57@gmail.com>
2023-03-05 18:49:56 +0900

    Problem25

---
+++ src/com/giseggi/dp/Problem25.java
@@ -0,0 +1,34 @@
+package com.giseggi.dp;
+
+import java.util.Scanner;
+
+public class Problem25 {
+	public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int D = sc.nextInt();
+        int K = sc.nextInt();
+        int dp[] = new int[D + 1];
+
+        dp[1] = 1;
+        dp[2] = 1;
+        for(int i = 3; i <= D; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+
+        int A = 1;
+        int B = 0;
+        boolean flg = true;
+
+        while(flg) {
+           if((K - (dp[D - 2] * A)) % dp[D - 1] == 0) {
+               B = (K - (dp[D - 2] * A)) / dp[D - 1];
+               flg = false;
+           } else {
+               A++;
+           }
+        }
+
+        System.out.println(A);
+        System.out.println(B);
+    }
+}
